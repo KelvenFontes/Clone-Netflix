1f1ed0db61c76d0c6154c4afc7ea99b87c821520
Kelven <kelvenbentofontes@gmail.com>
2022-10-10 21:31:49 -0300

    introduzindo imagens dos filmes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(()=>{
     const loadAll = async () =>{
       //pegando a lista total
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
     }
 
     loadAll();
   }, []);
 
   return (
-    <div>
-      <p>Olá Mundo!</p>
+    <div className="page">
+      <section className="lists">
+    
+        {movieList.map((item, key)=>(
+          <div>
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          </div>
+        ))}
+
+      </section>
     </div>
   );
 }
